a34045b83da5c783ae2e1e1b590c8d11905db50e
Jasson <jassonjasson17@gmail.com>
2025-07-16 15:19:49 +0700

    Calculate portfolio value and insert in in a table

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import json
 import os
-import yfinance 
+import yfinance
+from tabulate import tabulate
 
 file_path = "Stock-Tracker\portfolio.json"
 
@@ -13,6 +14,7 @@ def add_stock(ticker , quantity , buy_price):
         with open(file_path , mode="w") as file:
             json.dump(data, file)
 
+    ticker = ticker.upper()
     data[ticker] = {"quantity" : quantity , "buy_price": buy_price}
     with open(file_path , "w") as file:
         json.dump(data, file , indent = 2)
@@ -29,7 +31,37 @@ def get_live_price(ticker):
         return None
 
 def calculate_portfolio_value():
-    pass
+    total_value = 0
+    total_gain_loss = 0
+    all_data = [["Ticker", "Quantity", "Buy Price", "Current Price", "Gain/Loss"]]
+    with open(file_path , mode="r") as file:
+        data = json.load(file)
+        
+        for (key,value) in data.items():
+            stock_quantity = value["quantity"]
+            stock_buy_price = value["buy_price"]
+            stock_live_price = get_live_price(key)
+
+            if get_live_price is None:
+                continue
+
+            total = stock_quantity * stock_live_price
+            total_value += total
+            gain_loss = (stock_live_price - stock_buy_price) * stock_quantity
+            total_gain_loss += gain_loss
+
+            all_data.append([
+                key,
+                stock_quantity,
+                f"${stock_buy_price:.2f}",
+                f"${stock_live_price:.2f}",
+                f"${gain_loss:.2f}"
+            ])
+
+    print(tabulate(all_data , headers = "firstrow", tablefmt="grid"))
+    print(f"\nTotal Portfolio Value: ${total_value:.2f}")
+    print(f"Total Gain\Loss: {total_gain_loss:+.2f}")
+        
 
 def main():
     while True:
@@ -49,6 +81,10 @@ def main():
 
             elif user_input == 2:
                 calculate_portfolio_value()
+
+            elif user_input == 3:
+                print("Exiting the program...")
+                break
         except ValueError:
             print("Please input a valid menu.")
 
